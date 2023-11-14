ae72f61efbee52bb34b9bb1eae1d768564bc7db0
Sebastian Qian <148395429+qianq826@users.noreply.github.com>
2023-11-14 23:25:19 +0000

    updated

--- cli.py
+++ cli.py
@@ -1,72 +1,81 @@
 import random
 
 class Board:
-    def __init__(self):
-        self.board = [[" " for _ in range(3)] for _ in range(3)]
+  def __init__(self):
+    self._rows = [
+        [None, None, None],
+        [None, None, None],
+        [None, None, None],
+    ]
 
-    def display(self):
-        for row in self.board:
-            print("|".join(row))
-            print("-" * 5)
+  def __str__(self):
+    s = '-------\n'
+    for row in self._rows:
+      for cell in row:
+        s = s + '|'
+        if cell == None:
+          s=s+' '
+        else:
+          s=s+cell
+      s = s + '|\n-------\n'
+    return s
 
-    def is_full(self):
-        return all(all(cell != " " for cell in row) for row in self.board)
+  def get(self, x, y):
+    return self._rows[y][x]
 
-    def update_board(self, move, symbol):
-        if self.board[move[0]][move[1]] == " ":
-            self.board[move[0]][move[1]] = symbol
-            return True
-        return False
+  def set(self, x, y, value):
+    self._rows[y][x] = value
 
-    def check_winner(self, symbol):
+  def is_full(self):
+        return all(cell is not None for row in self._rows for cell in row)
+
+
+  def get_winner(self):
         # Check rows, columns, and diagonals for a win
         for i in range(3):
-            if all(self.board[i][j] == symbol for j in range(3)) or \
-               all(self.board[j][i] == symbol for j in range(3)):
+            if self._rows[i][0] == self._rows[i][1] == self._rows[i][2] is not None or \
+               self._rows[0][i] == self._rows[1][i] == self._rows[2][i] is not None:
                 return True
-        if self.board[0][0] == self.board[1][1] == self.board[2][2] == symbol or \
-           self.board[0][2] == self.board[1][1] == self.board[2][0] == symbol:
+        if self._rows[0][0] == self._rows[1][1] == self._rows[2][2] is not None or \
+           self._rows[0][2] == self._rows[1][1] == self._rows[2][0] is not None:
             return True
         return False
 
-class Player:
-    def get_move(self, board):
-        pass
+class Game:
+    def __init__(self, playerX, playerO):
+        self._board = Board()
+        self._playerX = playerX
+        self._playerO = playerO
+        self._current_player = self._playerX
+
+    def run(self):
+        while not self._board.is_full() and not self._board.get_winner():
+            print(self._board)
+            move = self._current_player.get_move(self._board)
+            x, y = move
+            if self._board.get(x, y) is None:
+                self._board.set(x, y, 'X' if self._current_player == self._playerX else 'O')
+                self._current_player = self._playerO if self._current_player == self._playerX else self._playerX
+            else:
+                print("Invalid move. Try again.")
+
+        print(self._board)
+        if self._board.get_winner():
+            print("We have a winner!")
+        else:
+            print("It's a draw!")
 
 class Human:
     def get_move(self, board):
-        move = input("Enter your move (row col): ").split()
+        move = input("Enter your move (x y): ").split()
         return int(move[0]), int(move[1])
 
 class Bot:
     def get_move(self, board):
-        empty_cells = [(i, j) for i in range(3) for j in range(3) if board.board[i][j] == " "]
-        return random.choice(empty_cells)
+        available_moves = [(x, y) for x in range(3) for y in range(3) if board.get(x, y) is None]
+        return random.choice(available_moves) if available_moves else (None, None)
 
-class Game:
-    def __init__(self, playerX, playerO):
-        self.board = Board()
-        self.playerX = playerX
-        self.playerO = playerO
-        self.players = [playerX, playerO]
-        self.current_player = 0
 
-    def run(self):
-        while True:
-            self.board.display()
-            move = self.players[self.current_player].get_move(self.board)
-            if self.board.update_board(move, "X" if self.current_player == 0 else "O"):
-                if self.board.check_winner("X" if self.current_player == 0 else "O"):
-                    print(f"Player {'X' if self.current_player == 0 else 'O'} wins!")
-                    break
-                if self.board.is_full():
-                    print("It's a draw!")
-                    break
-                self.current_player = 1 - self.current_player
-            else:
-                print("Invalid move, try again.")
-
-# Example usage
 player_choice = input("Choose 1 for single player or 2 for two players: ")
 if player_choice == "1":
     game = Game(Human(), Bot())
